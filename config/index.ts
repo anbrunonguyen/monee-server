98890de605bb0e0f0d0999b178ae026984fcbde0
Nguyen Thanh An <an.nguyen@difisoft.com>
2021-11-22 12:10:35 +0700

    Use express

---
+++ config/index.ts
@@ -0,0 +1,3 @@
+export const config = {
+  port: 2480,
+};
---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const port = process.env.PORT || config.port;
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
