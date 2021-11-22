69e33efd0235817946ceb767cb728d66f88c846d
Nguyen Thanh An <an.nguyen@difisoft.com>
2021-11-22 14:10:32 +0700

    New version

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 export default {
   port: 2480,
-  version: '0.1.3',
+  version: '0.1.4',
   dbUrl:
     'mongodb+srv://annguyen:annguyen@cluster0.k256i.gcp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
 };
